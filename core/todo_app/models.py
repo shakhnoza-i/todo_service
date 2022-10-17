e33fe565050a5707d6920adb5849ab39bfb1a328
shakhnoza-i <shaha0111@mail.ru>
2022-10-17 19:20:06 +0600

    permission testing

--- core/todo_app/models.py
+++ core/todo_app/models.py
@@ -5,7 +5,7 @@ class Task(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField(max_length=255)
     complete_time = models.DateTimeField()
-    completed = models.BooleanField(default=False)
+    # completed = models.BooleanField(default=False)
 
     def __str__(self): 
         return self.name
